93e8fcc10e94fb4087e80a8f15daf67c88bb4d67
loquenz <104274480+loquenz@users.noreply.github.com>
2022-04-29 00:01:54 -0400

    Update atencion a clientes.js

--- plugins/atencion a clientes.js
+++ plugins/atencion a clientes.js
@@ -2,10 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/insultar.mp3'
-conn.sendFile(m.chat, vn, 'insultar.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: true
+return m.reply('*Que basado/a eres Admin, no puedo eliminarte 😐*') 
 })
 }
 handler.customPrefix = /bot puto|Bot puto|Bot CTM|Bot ctm|bot CTM|bot ctm|bot pendejo|Bot pendejo|Bot gay|bot de mierda/ 
